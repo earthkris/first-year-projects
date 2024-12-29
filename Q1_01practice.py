d5a59e8aa6b7cf96533ee250ded6ede30bf9e054
earthkris <144130485+earthkris@users.noreply.github.com>
2024-12-29 20:28:18 +0700

    Delete Q1_01practice.py

--- Q1_01practice.py
+++
@@ -1,8 +0,0 @@
-h , m , s = input("Input : ").split(":")
-h , m , s = int(h) , int(m) , int(s)
-if 0 <= h <= 24 and  0 <= m <= 59 and  0 <= s <= 59: 
-    total = (h*60*60)+(m*60)+s 
-    print ("The number of seconds = %d" % total)
-
-else:
-    print("invalid time")
