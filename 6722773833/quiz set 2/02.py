430f46aea7de2bfac3b51ba24bf3781ed03f4e9d
earthkris <144130485+earthkris@users.noreply.github.com>
2024-12-29 20:34:15 +0700

    Delete 6722773833 directory

--- 6722773833/quiz set 2/02.py
+++
@@ -1,7 +0,0 @@
-age,name = input("Please enter your information: ").split(",")
-
-if age.isnumeric() and name.isalpha():
-    print("Your name is %s." %name)
-    print("Your age is %s." %age)
-else:
-    print("Please enter your complete information.")
